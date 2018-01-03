6eee97e5af320d80202b26b057fa86f0358af439
felbinger <nico.felbinger2000@gmail.com>
2018-01-03 15:51:33 +0100

    Create genpw.sh

---
+++ genpw.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+pw_lenght=20
+pw_count=1
+pw_keyset="A-Za-z0-9"
+
+while getopts ":l:c:k:" opt; do
+  case $opt in
+    l)
+      pw_lenght=${OPTARG}
+      ;;
+    c)
+      pw_count=${OPTARG}
+      ;;
+    k)
+      pw_keyset=${OPTARG}
+      ;;
+    *)
+      echo "Usage: $0 [OPTION]..."
+      echo "Print a random generated password. With no OPTION, $pw_count password with $pw_lenght characters ($pw_keyset)"
+      echo ""
+      echo "-l {int}             lenght of the passwords"
+      echo "-c {int}             count of the paswords"
+      echo "-k {String}          keyset of the password e.g. A-Z0-9"
+      exit 1
+      ;;
+  esac
+done
+
+cat /dev/urandom | tr -dc $pw_keyset | fold -w $pw_lenght | head -n $pw_count
+exit 0
