8c667b69f6f2584bf13af1542724d6fd716a960a
Nico <nico.felbinger2000@gmail.com>
2020-04-09 00:18:41 +0200

    Improved sorting

--- docker-networks.py
+++ docker-networks.py
@@ -15,15 +15,16 @@ def get_networks():
                     network.attrs.get('Name'),
                     subnet
                 ])
-
     return networks
 
 
 if __name__ == "__main__":
+    networks = sorted(get_networks(), key=lambda ip: list(map(int, ip[1].split("/")[0].split("."))))
+
     table = PrettyTable()
     table.title = "Docker Networks"
     table.field_names = ["Name", "Subnet"]
-    for row in get_networks():
+    for row in networks:
         table.add_row(row)
 
     table.align['Name'] = 'l'
